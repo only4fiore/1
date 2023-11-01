6508542d92da1d5b6af037d8e0989e96dd885a0e
only4fiore <only4fiore@gmail.com>
2023-11-01 18:49:49 +1000

    index

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
+export const getRemainder = (a, b) => a % b;
+ 
+export const calcPercentage = (a, b) => (100 * a) / b;
+
+export const getLongestString = (a, b) => (a.length >= b.length ? a : b);
+
+export const concatStrings = (a, b, i) => a.slice(0, i) + b + a.slice(i);
+
+export const normalizeVowels = (text) => {
+    const vowels = ['a', 'A', 'e', 'E', 'y', 'Y', 'u', 'U', 'i', 'I', 'o', 'O'];
+    let result = "";
+    for (let i = 0; i < text.length; i += 1) {
+        const char = text[i];
+        if (vowels.includes(char)) {
+            result += char.toLowerCase();
+        } else {
+            result += char;
+        }
+    }
+    return result;
+};
 
